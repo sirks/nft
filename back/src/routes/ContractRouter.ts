587f35cca8c8f0a171de522242700f9f0cd09127
kriss <kriss.pujats@gmail.com>
2022-02-20 16:26:21 +0200

    backend endpoints

---
+++ back/src/routes/ContractRouter.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {ownerOf, tokensOf, tokenURI} from "../contractUtils";
+
+const router = express.Router();
+
+router.get('/tokens-of/:address', async (req, res) => {
+    res.send(await tokensOf(req.params.address));
+});
+
+router.get('/token-uri/:token', async (req, res) => {
+    res.send(await tokenURI(req.params.token));
+});
+router.get('/owner-of/:token', async (req, res) => {
+    res.send(await ownerOf(req.params.token));
+});
+
+export default router;
