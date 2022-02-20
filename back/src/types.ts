afde6288eedd88ab909c582eec2fbce80a81714f
kriss <kriss.pujats@gmail.com>
2022-02-20 18:56:18 +0200

    validate qr against server

--- back/src/types.ts
+++ back/src/types.ts
@@ -1,18 +1,18 @@
 export type BaseEntity = { _id?: string, ts?: number }
-export type AccessEntity = BaseEntity & { event: string, token: string, address?: string, processing?: boolean}
+export type AccessEntity = BaseEntity & { event: string, token: string, address?: string, processing?: boolean }
+
+export enum ERR {
+    INCORRECT_DATA = -1,
+    NO_SUCH_TOKEN = -2,
+    TOKEN_USED = -3,
+    TOKEN_STOLEN = -4,
+}
+
+export enum OK {
+    NEW_VISIT = 1,
+    RETURNING_VISIT = 2
+}
 
 type HttpOk = { data: any }
-type HttpError = { err: { msg: string, code: number } }
+type HttpError = { err: { msg: string, code: ERR } }
 export type BaseRestResp = HttpOk | HttpError;
-
-export const ERR = {
-    INCORRECT_DATA: -1,
-    NO_SUCH_TOKEN: -2,
-    TOKEN_USED: -3,
-    TOKEN_STOLEN: -4,
-}
-
-export const OK = {
-    NEW_VISIT: 1,
-    RETURNING_VISIT: 2
-}
